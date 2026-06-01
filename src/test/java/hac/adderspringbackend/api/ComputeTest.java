f8ffd8fc5561b61b6688ca2b024283dba0d167e6
Sol <sodanabe@gmail.com>
2026-06-01 23:47:27 +0300

    springboot version 4

--- src/test/java/hac/adderspringbackend/api/ComputeTest.java
+++ src/test/java/hac/adderspringbackend/api/ComputeTest.java
@@ -1,10 +1,10 @@
 package hac.adderspringbackend.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import hac.adderspringbackend.dto.ComputeOperands;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
